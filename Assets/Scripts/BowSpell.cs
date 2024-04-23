97a977fedf2c9cddc46acb4ed2d527bdfb6e046c
Mateus Aurelio <83532421+Mateus-Aurelio@users.noreply.github.com>
2024-04-23 17:43:59 -0400

    Build Setup

---
+++ Assets/Scripts/BowSpell.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowSpell : Spell
+{
+    void Start()
+    {
+        
+    }
+
+    public override void SpellInit(PlayerHand mainHand)
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BowSpellHandSpot.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowSpellHandSpot : MonoBehaviour
+{
+    [SerializeField] private BowSpell bow;
+    [SerializeField] private bool gripHandSpot;
+    [SerializeField] private bool stringHandSpot;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
