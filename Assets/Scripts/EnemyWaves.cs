e8fec9d8b134a8c28f95d4ea1de1f8df625da437
Mateus Aurelio <83532421+Mateus-Aurelio@users.noreply.github.com>
2024-04-25 05:34:39 -0400

    New Scenes + Choppy Variants

---
+++ Assets/Scripts/EnemyWaves.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaves : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> wave1 = new List<GameObject>();
+    [SerializeField] private List<GameObject> wave2 = new List<GameObject>();
+    [SerializeField] private List<GameObject> wave3 = new List<GameObject>();
+    [SerializeField] private List<GameObject> wave4 = new List<GameObject>();
+
+    private List<GameObject> livingEnemies = new List<GameObject>();
+
+    float timer = 0;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer > 0) return;
+        timer = 1;
+        foreach (GameObject g in livingEnemies)
+        {
+            if (g == null)
+            {
+                CleanList();
+                break;
+            }
+        }
+    }
+
+    private void CleanList()
+    {
+        List<GameObject> newList = new List<GameObject>();
+
+        foreach (GameObject g in livingEnemies)
+            if (g != null) newList.Add(g);
+        livingEnemies = newList;
+    }
+}
