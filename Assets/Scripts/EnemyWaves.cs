70fe5332b3df6febe47200f5315b6403949856da
Mateus Aurelio <83532421+Mateus-Aurelio@users.noreply.github.com>
2024-04-25 07:19:43 -0400

    Enemy Waves

--- Assets/Scripts/EnemyWaves.cs
+++ Assets/Scripts/EnemyWaves.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyWaves : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class EnemyWaves : MonoBehaviour
     [SerializeField] private List<GameObject> wave2 = new List<GameObject>();
     [SerializeField] private List<GameObject> wave3 = new List<GameObject>();
     [SerializeField] private List<GameObject> wave4 = new List<GameObject>();
+    [SerializeField] private List<GameObject> wave5 = new List<GameObject>();
+    [SerializeField] private List<GameObject> wave6 = new List<GameObject>();
+    [SerializeField] private List<Transform> enemySpawnPoints = new List<Transform>();
+    private int spawnPointIndex = 0;
+    [SerializeField] private GameObject fadeObject;
+    [SerializeField] private float fadeStartDelay = 3;
+    [SerializeField] private string sceneToLoad = "ShowcaseWin";
+    [SerializeField] private float sceneLoadDelay = 5;
 
     private List<GameObject> livingEnemies = new List<GameObject>();
 
@@ -32,6 +41,8 @@ public class EnemyWaves : MonoBehaviour
                 break;
             }
         }
+        if (livingEnemies.Count > 0) return;
+        NextWave();
     }
 
     private void CleanList()
@@ -41,5 +52,54 @@ public class EnemyWaves : MonoBehaviour
         foreach (GameObject g in livingEnemies)
             if (g != null) newList.Add(g);
         livingEnemies = newList;
+    }
+
+    private void NextWave()
+    {
+        List<GameObject> wave = new List<GameObject>();
+        waveNumber++;
+        switch (waveNumber)
+        {
+            case 0:
+                wave = wave1;
+                break;
+            case 1:
+                wave = wave2;
+                break;
+            case 2:
+                wave = wave3;
+                break;
+            case 3:
+                wave = wave4;
+                break;
+            case 4:
+                wave = wave5;
+                break;
+            case 5:
+                wave = wave6;
+                break;
+            case 6:
+                StartCoroutine("FadeStartDelay");
+                StartCoroutine("SceneChangeDelay");
+                break;
+        }
+        foreach (GameObject g in wave)
+        {
+            spawnPointIndex++;
+            if (spawnPointIndex >= enemySpawnPoints.Count) spawnPointIndex = 0;
+            livingEnemies.Add(Instantiate(g, enemySpawnPoints[spawnPointIndex].position, Quaternion.identity));
+        }
+    }
+
+    private IEnumerator FadeStartDelay()
+    {
+        yield return new WaitForSeconds(fadeStartDelay);
+        fadeObject.SetActive(true);
+    }
+
+    private IEnumerator SceneChangeDelay()
+    {
+        yield return new WaitForSeconds(sceneLoadDelay);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
